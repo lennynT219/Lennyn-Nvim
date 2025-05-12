25982540748613147cb69bffa3469d9201de4ef4
Lenin Ariel Masapanta Vega <lmasapanta@coopepn.local>
2025-05-12 09:12:05 -0500

    add: Terminal config

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 require("config.lazy")
 
 vim.opt.shell = "pwsh"
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
