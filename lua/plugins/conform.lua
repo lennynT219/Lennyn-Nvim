a35614d35d99e2cd53096b193ff2e98622ddfe59
LennynT219 <lennynvega35@gmail.com>
2025-09-08 14:04:03 -0500

    add: Formato actualizado para CS

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      cs = { "csharpier" },
+    },
+    formatters = {
+      csharpier = {
+        command = "csharpier",
+        args = { "format", "$FILENAME" },
+        stdin = false,
+      },
+    },
+  },
+}
