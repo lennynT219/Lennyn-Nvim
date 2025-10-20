7fad3158ddc54b847d9cc591c6a0178bc4108dfb
LennynT219 <lennynvega35@gmail.com>
2025-10-20 10:51:37 -0500

    add: Omitir error en Rust

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -13,4 +13,24 @@ dap.adapters.codelldb = {
   },
 }
 
-return {}
+return {
+  "mrcjkb/rustaceanvim",
+  ft = { "rust" },
+  opts = {
+    server = {
+      settings = {
+        -- rust-analyzer language server configuration
+        ["rust-analyzer"] = {
+          procMacro = {
+            enable = true,
+            ignored = {
+              -- ["async-trait"] = { "async_trait" },
+              ["napi-derive"] = { "napi" },
+              ["async-recursion"] = { "async_recursion" },
+            },
+          },
+        },
+      },
+    },
+  },
+}
