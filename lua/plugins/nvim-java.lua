d1949d2b4c2ac68510625a0682157ffe455d15d9
LennynT219 <lennynvega35@gmail.com>
2026-03-10 15:07:19 -0500

    add: Lazy extras adicionales

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,25 +1,28 @@
 return {
   "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            -- Your custom jdtls settings goes here
-          },
-        },
-        setup = {
-          jdtls = function()
-            require("java").setup({
-              jdk = {
-                auto_install = false,
-              },
-            })
-          end,
-        },
-      },
-    },
-  },
+  config = function()
+    require("java").setup()
+    vim.lsp.enable("jdtls")
+  end,
+  -- dependencies = {
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     opts = {
+  --       servers = {
+  --         jdtls = {
+  --           -- Your custom jdtls settings goes here
+  --         },
+  --       },
+  --       setup = {
+  --         jdtls = function()
+  --           require("java").setup({
+  --             jdk = {
+  --               auto_install = false,
+  --             },
+  --           })
+  --         end,
+  --       },
+  --     },
+  --   },
+  -- },
 }
