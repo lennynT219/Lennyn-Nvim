45a00080d89948a36cbfc08cacbf05e2c9ae0b19
LennynT219 <lennynvega35@gmail.com>
2026-03-20 03:34:18 -0500

    add: Merge con Linux

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,18 +1,3 @@
-local dap = require("dap")
-
-local codelldb_path =
-  "C:\\Users\\lmasapanta\\AppData\\Local\\nvim-data\\mason\\packages\\codelldb\\extension\\adapter\\codelldb.exe"
-
-dap.adapters.codelldb = {
-  type = "server",
-  host = "127.0.0.1",
-  port = "${port}", -- puedes generar dinámico como te mostré antes
-  executable = {
-    command = codelldb_path,
-    args = { "--port", "${port}" },
-  },
-}
-
 return {
   "mrcjkb/rustaceanvim",
   ft = { "rust" },
